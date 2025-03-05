c2e86646e96acacd004d43c2374ea97c6434c5d5
unknown <sachinchakradhar8@gmail.com>
2025-03-05 18:15:16 +0545

    changes

--- Backend/app.py
+++ Backend/app.py
@@ -1,32 +1,67 @@
+# from flask import Flask, send_from_directory
+# from flask_cors import CORS
+# from flask_sqlalchemy import SQLAlchemy
+# from db import db
+# import os
+
+# # app=Flask(__name__)
+# app = Flask(__name__, static_folder='../Frontend/dist', static_url_path='/')
+# CORS(app)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog_posts.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+
+# frontend_folder = os.path.join(os.getcwd(), '../Frontend/dist')
+# dist_folder = os.path.join(frontend_folder, 'dist')
+
+# @app.route('/', defaults={'filename': ''})
+# @app.route('/<path:filename>')
+# def index(filename):
+#     if not filename:
+#         return send_from_directory(frontend_folder, 'index.html')
+#     return send_from_directory(dist_folder, filename)
+
+# import routes
+
+# with app.app_context():
+#     db.create_all()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
 from flask import Flask, send_from_directory
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from db import db
 import os
 
-# app=Flask(__name__)
-app = Flask(__name__, static_folder='../Frontend/dist', static_url_path='/')
+app = Flask(__name__, static_folder='dist', static_url_path='/')
 CORS(app)
 
+# Database configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog_posts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-frontend_folder = os.path.join(os.getcwd(), '../Frontend/dist')
-dist_folder = os.path.join(frontend_folder, 'dist')
-
+# Serve React Frontend
 @app.route('/', defaults={'filename': ''})
 @app.route('/<path:filename>')
 def index(filename):
-    if not filename:
-        return send_from_directory(frontend_folder, 'index.html')
-    return send_from_directory(dist_folder, filename)
+    if filename == "":
+        return send_from_directory(app.static_folder, 'index.html')
+    return send_from_directory(app.static_folder, filename)
 
-import routes
+# Import routes after app is created
+import routes  
 
+# Create database tables
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
-    app.run()
+# Export the app for Vercel
+def handler(req, res):
+    return app
